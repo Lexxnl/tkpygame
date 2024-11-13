5dba561d62c3cc4d688174daf484a6e48b226476
Lex Kimmel <lex.kimmel@gmail.com>
2024-11-13 10:51:29 +0100

    Add files via upload

---
+++ examples/colored_terminal_messages_example.py
@@ -0,0 +1,4 @@
+from tkpygame import TerminalColors, print_colored
+
+print_colored("This is a warning message", TerminalColors.WARNING)
+print_colored("This is an error message", TerminalColors.FAIL)
