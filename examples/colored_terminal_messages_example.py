5b05795d895e213f8881dcd3c4f945a04dc6fe2f
Lex Kimmel <lex.kimmel@gmail.com>
2024-12-15 10:15:07 +0100

    Delete examples directory

--- examples/colored_terminal_messages_example.py
+++
@@ -1,4 +0,0 @@
-from tkpygame import TerminalColors, print_colored
-
-print_colored("This is a warning message", TerminalColors.WARNING)
-print_colored("This is an error message", TerminalColors.FAIL)
